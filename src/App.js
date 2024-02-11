d813c192647982bdf87d202950f4448d43eedf27
JosephSmithMCA <152203982+JosephSmithMCA@users.noreply.github.com>
2024-02-10 22:55:33 -0600

    added initial body
    preparing to add more components for future useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import react from "react";
 import Header from "./components/Header";
+import InitialBody from "./components/InitialBody";
 import Body from "./components/Body";
 import './style.css'
 
@@ -7,6 +8,7 @@ function App(){
 
     return(
         <div className="row">
+            <InitialBody/>
             <Header/>
             <Body/>
         </div>
---
+++ src/components/InitialBody.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function InitialBody(){
+
+    return(
+        <div className="row">
+            <InitialInitiativeDetails/>
+        </div>
+    )
+}
+
+function InitialInitiativeDetails(){
+
+    return(
+        <div className="col-sm-12">
+            <h1>Enter Combat Details</h1>
+        </div>
+    )
+}
+
+export default InitialBody
